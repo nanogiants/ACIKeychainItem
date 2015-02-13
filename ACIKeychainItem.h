e4547e5bb547991b0c235c469193bd25993b9619
Ralph-Gordon Paul <g.paul@appcom-interactive.de>
2015-02-13 16:24:55 +0100

    added first version

---
+++ ACIKeychainItem.h
@@ -0,0 +1,108 @@
+/*
+-------------------------------------------------------------------------------
+The MIT License (MIT)
+
+Copyright (c) 2015 appcom interactive GmbH. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+-------------------------------------------------------------------------------
+*/
+
+#import <Foundation/Foundation.h>
+
+typedef enum : NSUInteger {
+    ACIKeychainItemSecTypeAccount,     /**< usage: account / username       */
+    ACIKeychainItemSecTypeData,        /**< usage: password / data          */
+    ACIKeychainItemSecTypeLabel,       /**< usage: user visible label       */
+    ACIKeychainItemSecTypeDescription, /**< usage: user visible description */
+    ACIKeychainItemSecTypeGeneric
+} ACIKeychainItemSecType;
+
+/**
+ @brief Keychain Wrapper for iOS Keychain.
+ @details Provides an easy interface for basic Keychain usage. An ACIKeychain
+          represents a keychain item.
+ */
+@interface ACIKeychainItem : NSObject
+
+///< Identifier of the keychain item - internally this is kSecAttrService
+@property (nonatomic, strong, readonly) NSString *identifier;
+///< The access group of this keychain item
+@property (nonatomic, strong, readonly) NSString *accessGroup;
+
+/**
+ @brief Creates a keychain item.
+ @details
+ @param identifier obvious identity of that item (uses kSecAttrService)
+ @param accessGroup (optional) access group for sharing between apps.
+ */
+- (id)initWithIdentifier:(NSString *)identifier accessGroup:(NSString *)accessGroup;
+
+/**
+ @brief Inserts data for the corresponding type.
+ @details If there is already data for this type, this function will return false
+          and you have to call updateData:forSecType: instead.
+ @param data The data that will be added.
+ @param type The type of the data.
+ @return true on success and false on failure.
+ @sa updateData:accessGroup: dataForSecType:
+ */
+- (BOOL)insertData:(NSData *)data forSecType:(ACIKeychainItemSecType)type;
+
+/**
+ @brief Finds the data for the corresponding type.
+ @param data The new data.
+ @param type The type of the data.
+ @return true on success and false on failure.
+ @sa insertData:accessGroup: dataForSecType:
+ */
+- (BOOL)updateData:(NSData *)data forSecType:(ACIKeychainItemSecType)type;
+
+/**
+ @brief Inserts or updates the given data for the corresponding type.
+ @details First it tries to insert the data. If insertion failed due to already
+          existing data, it will update the current data with the new one 
+          instead.
+ @param data The data that will be added or updated.
+ @param type The type of the data.
+ @return true on success and false on failure.
+ @sa insertData:accessGroup: updateData:accessGroup: dataForSecType:
+ */
+- (BOOL)insertOrUpdateData:(NSData *)data forSecType:(ACIKeychainItemSecType)type;
+
+/**
+ @brief Finds the data for the corresponding type.
+ @param type The type of the data to return.
+ @return the data on success and nil on failure.
+ @sa updateData:accessGroup: insertData:accessGroup:
+ */
+- (NSData *)dataForSecType:(ACIKeychainItemSecType)type;
+
+/**
+ @brief Deletes the item from the keychain.
+ */
+- (BOOL)remove;
+
+/**
+ @brief Function to get the Bundle Seed ID programmatically.
+ @return Bundle Seed ID on success and nil on failure.
+ */
++ (NSString *)bundleSeedID;
+
+@end
---
+++ ACIKeychainItem.m
@@ -0,0 +1,221 @@
+/*
+-------------------------------------------------------------------------------
+The MIT License (MIT)
+
+Copyright (c) 2015 appcom interactive GmbH. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+-------------------------------------------------------------------------------
+*/
+
+#import "ACIKeychainItem.h"
+#import <Security/Security.h>
+
+@implementation ACIKeychainItem
+
+- (id)initWithIdentifier:(NSString *)identifier accessGroup:(NSString *)accessGroup
+{
+    NSAssert(identifier != nil, @"identifier has to be set.");
+    self = [super init];
+    if (self) {
+        _identifier = identifier;
+        _isAlwaysAccessible = YES;
+        
+#if !TARGET_IPHONE_SIMULATOR
+        // Don't use access group on simulator.
+        _accessGroup = accessGroup;
+#endif
+    }
+    return self;
+}
+
+- (BOOL)insertData:(NSData *)data forSecType:(ACIKeychainItemSecType)type
+{
+    NSMutableDictionary *query = [self createQuery];
+    
+    CFTypeRef cftype = [ACIKeychainItem attrTypeForKeychainItemSecType:type];
+    
+    if (cftype == NULL) return NO;
+    
+    query[(__bridge id)cftype] = data;
+    
+    OSStatus status = SecItemAdd((__bridge CFDictionaryRef)query, NULL);
+    
+    return (errSecSuccess == status);
+}
+
+- (BOOL)updateData:(NSData *)data forSecType:(ACIKeychainItemSecType)type
+{
+    NSMutableDictionary *query = [self createQuery];
+    NSMutableDictionary *update =[[NSMutableDictionary alloc] init];
+    CFTypeRef cftype = [ACIKeychainItem attrTypeForKeychainItemSecType:type];
+    
+    if (cftype == NULL) return NO;
+    
+    update[(__bridge id)cftype] = data;
+    
+    OSStatus status = SecItemUpdate((__bridge CFDictionaryRef)query, (__bridge CFDictionaryRef)update);
+    
+    return (errSecSuccess == status);
+}
+
+- (BOOL)insertOrUpdateData:(NSData *)data forSecType:(ACIKeychainItemSecType)type
+{
+    NSMutableDictionary *query = [self createQuery];
+    
+    CFTypeRef cftype = [ACIKeychainItem attrTypeForKeychainItemSecType:type];
+    
+    if (cftype == NULL) return NO;
+    
+    query[(__bridge id)cftype] = data;
+    
+    OSStatus status = SecItemAdd((__bridge CFDictionaryRef)query, NULL);
+    
+    // if item already exists - try to update
+    if (status == errSecDuplicateItem) {
+        NSMutableDictionary *update =[[NSMutableDictionary alloc] init];
+        update[(__bridge id)cftype] = data;
+        
+        status = SecItemUpdate((__bridge CFDictionaryRef)query, (__bridge CFDictionaryRef)update);
+    }
+    
+    return (errSecSuccess == status);
+}
+
+- (NSData *)dataForSecType:(ACIKeychainItemSecType)type
+{
+    NSMutableDictionary *query = [self createQuery];
+    CFTypeRef cftype = [ACIKeychainItem attrTypeForKeychainItemSecType:type];
+    
+    query[(__bridge id)kSecMatchLimit] = (__bridge id)kSecMatchLimitOne;
+    
+    // we need to gather data differently than other values
+    if (type == ACIKeychainItemSecTypeData) {
+        query[(__bridge id)kSecReturnData] = (id)kCFBooleanTrue;
+    } else {
+        query[(__bridge id)kSecReturnAttributes] = (id)kCFBooleanTrue;
+    }
+
+    CFTypeRef result = NULL;
+    OSStatus status = SecItemCopyMatching((__bridge CFDictionaryRef)query, (CFTypeRef *)&result);
+    
+    // if we failed - just memory management and return
+    if (status != errSecSuccess) {
+        if (result != NULL) CFRelease(result);
+        return nil;
+    }
+    
+    // if we gathered data, get them and return
+    if (type == ACIKeychainItemSecTypeData) {
+        NSData *data = [(__bridge NSData *)result copy];
+        CFRelease(result);
+        return data;
+    }
+    
+    // receive the data from the results
+    id data = [(__bridge NSDictionary *)result objectForKey:(__bridge id)cftype];
+    
+    CFRelease(result);
+    
+    // convert any strings to data
+    if ([data isKindOfClass:[NSString class]]) {
+        return [data dataUsingEncoding:NSUTF8StringEncoding];
+    }
+    
+    return data;
+}
+
+- (BOOL)remove
+{
+    NSMutableDictionary *query = [self createQuery];
+    OSStatus status = SecItemDelete((__bridge CFDictionaryRef)query);
+
+    return (errSecSuccess == status);
+}
+
+// we create a bundleSeedID generic value to gather the bundleSeedID from keychain
++ (NSString *)bundleSeedID
+{
+    NSDictionary *query = @{
+                            (__bridge id)kSecClass : (__bridge id)kSecClassGenericPassword,
+                            (__bridge id)kSecAttrGeneric : @"bundleSeedID",
+                            (__bridge id)kSecAttrService : @"",
+                            (__bridge id)kSecReturnAttributes : (id)kCFBooleanTrue
+                            };
+    
+    CFDictionaryRef result = NULL;
+    OSStatus status = SecItemCopyMatching((__bridge CFDictionaryRef)query, (CFTypeRef *)&result);
+    if (status == errSecItemNotFound)
+        status = SecItemAdd((__bridge CFDictionaryRef)query, (CFTypeRef *)&result);
+    if (status != errSecSuccess)
+        return nil;
+    NSString *accessGroup = [(__bridge NSDictionary *)result objectForKey:(__bridge NSData *)kSecAttrGeneric];
+    NSArray *components = [accessGroup componentsSeparatedByString:@"."];
+    NSString *bundleSeedID = [components firstObject];
+    CFRelease(result);
+    return bundleSeedID;
+}
+
+#pragma mark - Private
+
+- (NSMutableDictionary *)createQuery
+{
+    NSData *identifier = [self.identifier dataUsingEncoding:NSUTF8StringEncoding];
+    NSMutableDictionary *query = [[NSMutableDictionary alloc] init];
+    
+    query[(__bridge id)kSecClass] = (__bridge id)kSecClassGenericPassword;
+    query[(__bridge id)kSecAttrService] = identifier;
+    
+    if (self.accessGroup) query[(__bridge id)kSecAttrAccessGroup] = self.accessGroup;
+    
+    return query;
+}
+
+// returns corresponding cftype
++ (CFTypeRef)attrTypeForKeychainItemSecType:(ACIKeychainItemSecType)type
+{
+    switch (type)
+    {
+        case ACIKeychainItemSecTypeAccount:
+            return kSecAttrAccount;
+            break;
+            
+        case ACIKeychainItemSecTypeData:
+            return kSecValueData;
+            break;
+            
+        case ACIKeychainItemSecTypeLabel:
+            return kSecAttrLabel;
+            break;
+            
+        case ACIKeychainItemSecTypeDescription:
+            return kSecAttrDescription;
+            break;
+            
+        case ACIKeychainItemSecTypeGeneric:
+            return kSecAttrGeneric;
+            break;
+            
+        default: break;
+    }
+    
+    return NULL;
+}
+
+@end
